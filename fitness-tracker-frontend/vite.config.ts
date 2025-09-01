774ab51d8654db410af69c111b45508bab62615e
manueljsanchezg <manueljsanchez10@gmail.com>
2025-09-01 17:22:03 +0200

    fix icon naming

--- fitness-tracker-frontend/vite.config.ts
+++ fitness-tracker-frontend/vite.config.ts
@@ -17,12 +17,12 @@ export default defineConfig({
         theme_color: '#2c2c2c',
         icons: [
           {
-            src: '/icons/icon-192.png',
+            src: '/icons/athlo-icon-192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/icons/icon-512.png',
+            src: '/icons/athlo-icon-512.png',
             sizes: '512x512',
             type: 'image/png'
           }
