7a81ed66a27eb07efcdf133bacdb39b0358a06d4
manueljsanchezg <manueljsanchez10@gmail.com>
2025-09-01 13:13:56 +0200

    feat: env for backend connection

--- fitness-tracker-frontend/src/api/api.ts
+++ fitness-tracker-frontend/src/api/api.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 import { router } from "../router"
 
 export const server = axios.create({ 
-    baseURL: 'http://localhost:3000/api/v1',
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true
  })
 
